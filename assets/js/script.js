fc001528bae7dc2c8842d2397bbf819c9f56f698
dolcebasstrombone <dolcebasstrombone@gmail.com>
2022-05-16 12:50:11 -0400

    rearrange file nesting, add assets files, js pseudo code, and readme with requirements

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+//display current date and time at the top
+//user input handler
+//save to local storage
+//retrive from local storage
+//color coding based on past, present, or future
+
+//time blocks can be hard coded in html
+//blessed be simple column and row layouts for easy media queries, amen
